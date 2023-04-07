0f09650e5e104b8b1dae5abb3aa8680513028751
parixscm <parixscm@gmail.com>
2023-04-07 16:07:31 +0900

    📝 Feat: ADD date utils functionality

---
+++ src/utils/date.js
@@ -0,0 +1,23 @@
+/**
+ * 파일 역할: date 관련 유틸리티 함수 모음
+ * 작성자: Jason (parixscm)
+ * 최근 업데이트: 2023.04.07.
+ */
+
+// Convert milliseconds to seconds (API가 unix timestamp 사용하기 때문)
+export const convertDateToUnixTimestamp = date => {
+  return ~~(date.getTime() / 1000);
+};
+
+export const convertUnixTimestampToDate = unixTimestamp => {
+  const milliseconds = unixTimestamp * 1000;
+  return new Date(milliseconds).toLocaleDateString();
+};
+
+// Get start date or end date
+export const createDate = (date, days, weeks, months, years) => {
+  let newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days + 7 * weeks);
+  newDate.setMonth(newDate.getMonth() + months);
+  newDate.setFullYear(newDate.getFullYear() + years);
+};
